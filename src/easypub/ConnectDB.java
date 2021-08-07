5dab8356cb945f24da606a29def004b36d3a4ffb
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-08-07 15:38:13 +0100

    Create ConnectDB.java

---
+++ src/easypub/ConnectDB.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easypub;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author vanel
+ */
+public class ConnectDB {
+
+    public static Connection connect() {
+
+        Connection cnct = null;
+        String url = "jdbc:mysql://localhost:3306/easypubdatabase?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        String user = "root";
+        String password = "";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cnct = DriverManager.getConnection(url, user, password);            
+        } catch (SQLException ex) {
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+        return cnct;
+    }
+}
