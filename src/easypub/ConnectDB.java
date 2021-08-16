fd69887fbd14b326e83b5e83b212c2eb4c52a726
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-08-16 11:14:44 +0100

    Update ConnectDB.java

--- src/easypub/ConnectDB.java
+++ src/easypub/ConnectDB.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Class connects system to database
  */
 package easypub;
 
@@ -15,7 +13,7 @@ public class ConnectDB {
     public static Connection connect() {
 
         Connection cnct = null;
-        String url = "jdbc:mysql://localhost:3306/easypubdatabase?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        String url = "jdbc:mysql://localhost:3306/easypubdatabase?zeroDateTimeBehavior=CONVERT_TO_NULL";//database adress
         String user = "root";
         String password = "";
 
